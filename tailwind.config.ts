e51a6ee856185b7cfd971344532094f48e079f23
Aastha-Kalra <126173305+Aastha-Kalra@users.noreply.github.com>
2024-04-08 23:08:41 +0530

    updated frontend part

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
+      outline: {
+        gradient: ['4px solid', 'linear-gradient(to bottom, #CE722C, #471514)'],
+      },
     },
   },
   plugins: [],
